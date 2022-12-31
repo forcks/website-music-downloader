b96621a095c0cd35bc96acff09df801e5e5eb8f1
Sergey <devsergo3@gmail.com>
2022-12-31 12:52:37 +0300

    add comments

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from bs4 import BeautifulSoup as b
 import requests
 import os
 
-URL = "https://ruo.morsmusic.org/artist/"
+#link to the site from which the music will be downloaded
 url_site = "https://ruo.morsmusic.org"
+# example https://ruo.morsmusic.org/artist/973
 id = "973"
+#folder where the music will be downloaded
 pathToMusicFolder = "C:\\Users\\Sergey\\Desktop\\music\\"
 
+Pages = 4
+
+#a function that receives blocks with music
 def parse(url, block, _class):
     r = requests.get(url)
     print(r.status_code)
@@ -17,12 +22,15 @@ def parse(url, block, _class):
 
 
 def downloadMusic():
-    music_block = parse(URL + id, 'div', 'track mustoggler __adv_list_track')
+    #getting music from the first page
+    music_block = parse(url_site+"/artist/"+ id, 'div', 'track mustoggler __adv_list_track')
 
+    #if there are more than 1 pages with music, then they are sorted according to the variable
     try:
+        #enumeration starts from page 2 (i = 2)
         i = 2
-        while i<= 4:
-            music_block+=parse(URL + id+"?page="+str(i), 'div', 'track mustoggler __adv_list_track')
+        while i<= Pages:
+            music_block+=parse(url_site+"/artist/"+ id+"?page="+str(i), 'div', 'track mustoggler __adv_list_track')
             i+=1
     except BaseException:
         print("empty")
@@ -31,6 +39,8 @@ def downloadMusic():
 
     count = 0
     while count <= len(music_block):
+        #getting track data
+
         info = music_block[int(count) - 1]
         link = info.find('a', {"class": "track-download"}).get("href")
         name = info.find('a', {"class": "media-link media-name"}).text
@@ -44,6 +54,7 @@ def downloadMusic():
 
         path = pathToMusicFolder
 
+        #create folder with artist name
         if not os.path.isdir(path+artist+"\\"):
             os.mkdir(path+artist)
 
